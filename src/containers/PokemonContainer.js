98340271a8e3de30c1948a01a36d0beac683998d
Marco Chavez <marcode.chavez.jr@gmail.com>
2020-06-14 22:42:09 -0700

    added styling

--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -6,7 +6,7 @@ import { GET_POKEMONS } from '../graphql/get-pokemon';
 export default function PokemonContainer() {
 
     const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
-        variables: { first: 9 }
+        variables: { first: 24 }
     });
 
     return (
