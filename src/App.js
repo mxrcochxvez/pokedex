efda3626f2558b59e441da6ef6acc487c20efc5c
Marco Chavez <marcode.chavez.jr@gmail.com>
2020-06-14 21:46:15 -0700

    setting up apollo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import './App.css';
 
+import PokemonContainer from './containers/PokemonContainer';
+
 function App() {
   const client = new ApolloClient({
     uri: 'https://graphql-pokemon.now.sh/'
@@ -10,7 +12,9 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-      <p>It's working!</p>
+      <main>
+        <PokemonContainer />
+      </main>
     </ApolloProvider>
   );
 }
